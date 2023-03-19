0a23881265f891a0e48689617b749fa44218e1dd
Trqhxrd <trqhxrd@duck.com>
2023-03-19 10:19:53 +0100

    Removed the start of debug code.

--- src/main/kotlin/me/trqhxrd/octorpg/Main.kt
+++ src/main/kotlin/me/trqhxrd/octorpg/Main.kt
@@ -1,8 +1,6 @@
 package me.trqhxrd.octorpg
 
 import me.trqhxrd.octorpg.api.OctoRPG
-import me.trqhxrd.octorpg.debug.PlayerJoinListener
-import org.bukkit.Bukkit
 import org.bukkit.plugin.PluginDescriptionFile
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.plugin.java.JavaPluginLoader
@@ -16,16 +14,10 @@ class Main : JavaPlugin {
     constructor() : super()
 
     constructor(loader: JavaPluginLoader, description: PluginDescriptionFile, dataFolder: File, file: File) :
-            super(
-                loader,
-                description,
-                dataFolder,
-                file
-            )
+            super(loader, description, dataFolder, file)
 
     override fun onEnable() {
         this.instance = OctoRPG(this)
-        Bukkit.getPluginManager().registerEvents(PlayerJoinListener(this.instance), this)
 
         this.logger.info("Hello World!")
     }
